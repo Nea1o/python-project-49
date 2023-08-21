e1b9fcaa471efeb600179cca276d9f5ba4af2d63
Andrey Nikel <nikelandrey94@gmail.com>
2023-08-21 13:35:46 +0600

    task 7th add brain-gcd + acsiinema brain-gcd

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [![Code Climate](https://camo.githubusercontent.com/13fac8fbe2bfb7af717785f6c9ff0bf089d5033bc4b86e955d0f61bbf40b52d4/68747470733a2f2f636f6465636c696d6174652e636f6d2f6769746875622f636c6f7564666f756e6472792f6d656d6272616e652e706e67)](https://codeclimate.com/github/Nea1o/python-project-49)
 [![Static Badge](https://img.shields.io/badge/records-brain_even-blue)](https://asciinema.org/a/3HijhIAPJCQAEZhfd2LzgjZXD)
 [![Static Badge](https://img.shields.io/badge/records-brain_calc-blue)](https://asciinema.org/a/8CgESU1MCkJjnm8qJi8YQIJwx)
+[![Static Badge](https://img.shields.io/badge/records-brain_gcd-blue)](https://asciinema.org/a/NAvMdoHm27T7XBxHJXNsAnYbZ)
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,34 @@
+from brain_games.cli import welcome_user
+from brain_games.cli import comparison_of_answer
+import random
+import prompt
+
+
+def main():
+    name = welcome_user()
+    print('Find the greatest common divisor of given numbers.')
+    count = 0
+    result = ""
+    while count < 3:
+        num_1 = random.randint(1, 100)
+        num_2 = random.randint(1, 100)
+        print(f'Question: {num_1} {num_2}')
+        answer = prompt.string("your answer? ")
+        min_value = min([num_1, num_2])
+        for elem in list(range(min_value+1))[::-1]:
+            if num_1 % elem == 0 and num_2 % elem == 0:
+                true_answer =  str(elem)
+                break
+        result = comparison_of_answer(answer, true_answer, name)
+        if result == 'correct':
+            count += 1
+        else:
+            break
+        if count == 3:
+            result = f"Congratulations, {name}!"
+            break
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
