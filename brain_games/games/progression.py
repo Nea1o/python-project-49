b24f7e0b418d6bc1db3e44b4117d667597116d22
Andrey Nikel <nikelandrey94@gmail.com>
2023-11-01 12:02:58 +0600

    fix

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -12,7 +12,7 @@ def executing_cycle_progression():
     secret_number = random.randint(MIN_SECRET_NUM, len_progression)
     true_answer = str(list_question[secret_number])
     list_question[secret_number] = ".."
-    print('Question: ', ' '.join(map(str, list_question)))
+    print('Question:', ' '.join(map(str, list_question)))
     return true_answer
 
 
@@ -28,6 +28,6 @@ MIN_NUM_BEGIN = 1
 MAX_NUM_BEGIN = 20
 STEP_MIN = 1
 STEP_MAX = 10
-LEN_MIN = 4
+LEN_MIN = 5
 LEN_MAX = 14
 MIN_SECRET_NUM = 0
